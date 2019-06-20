ed51562825de0be14ed88896e2ca5701d814e8ef
sundeep <pidugusundeep5@gmail.com>
2019-06-20 08:10:35 +0530

    Host name detaults

--- Video Downloader/background.js
+++ Video Downloader/background.js
@@ -11,7 +11,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        // pageUrl: {hostEquals: 'developer.chrome.com'},
+        pageUrl: {hostEquals: 'acloud.guru'},
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
